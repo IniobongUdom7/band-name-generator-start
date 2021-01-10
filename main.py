0cb033ea7071e647fba0bd1b901f4a2f8a1c32a4
Iniobong Udom <63455287+IniobongUdom7@users.noreply.github.com>
2021-01-11 00:44:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 #1. Create a greeting for your program.
 print("Welcome to the brand name generator")
 #2. Ask the user for the city that they grew up in.
-street = input("which city did you grow up?\n")
+Street = input("which city did you grow up?\n")
 #3. Ask the user for the name of a pet.
-pet = input("what is your pet name?\n")
+Pet = input("what is your pet name?\n")
 
 #5. ask the user the goods the deal on.
-goods = input("what type of goods do you deal on?\n")
+Goods = input("what type of goods do you deal on?\n")
 #4. Combine the name of their city,pet and goods to show their brand name..
-print("your brand name could be " + street + " " + pet + " " + goods)
+print("your brand name could be " + street + " " + Pet + " " + Goods)
 
